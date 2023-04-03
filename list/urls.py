ed4c9961dafcc6f7c7b42d103d11cd5f445977dd
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-04-03 14:57:28 +0300

    Fix indents in urls.py

--- list/urls.py
+++ list/urls.py
@@ -8,27 +8,16 @@ from list.views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
-    TaskDeleteView, task_completion
+    TaskDeleteView,
+    task_completion
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags", TagListView.as_view(), name="tag-list"),
-    path(
-        "tags/create/",
-        TagCreateView.as_view(),
-        name="tag-create",
-    ),
-    path(
-        "tags/<int:pk>/update/",
-        TagUpdateView.as_view(),
-        name="tag-update",
-    ),
-    path(
-        "tags/<int:pk>/delete/",
-        TagDeleteView.as_view(),
-        name="tag-delete",
-    ),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("/create/", TaskCreateView.as_view(), name="task-create"),
     path("/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
