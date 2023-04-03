9eec6d302344e756a55fa3ca8dfb73d2608d9a37
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-04-03 11:14:15 +0300

    Create Tag and Task models, run the migrations

--- list/models.py
+++ list/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.content
